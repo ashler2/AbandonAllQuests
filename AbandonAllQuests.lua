d2fc3787fd19cb74f0816becfa8a97dff8cbfc65
ashler2 <ashley.rt@live.co.uk>
2020-11-23 09:56:07 +0000

    abandon quest triggers

--- AbandonAllQuests.lua
+++ AbandonAllQuests.lua
@@ -12,13 +12,14 @@ local function AbandonAllQuestsHandler()
     for i = 0,getMaxQuests(),1 
     do 
         local id = C_QuestLog.GetQuestIDForLogIndex(i)
-        if(id != 0 )then
+        if(id > 0 )then
             if(C_QuestLog.CanAbandonQuest(id)) then
-                print('yes')
+                C_QuestLog.SetSelectedQuest(id)
+                C_QuestLog.SetAbandonQuest()
+                C_QuestLog.AbandonQuest()
 
             end
         end
-        print(id)
     end
 
 end
