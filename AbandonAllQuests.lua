467961a0419bd086fcedbd4ee3c63b0d14b3dbf9
ashler2 <ashley.rt@live.co.uk>
2020-11-23 09:50:27 +0000

    checking quest can be abandoned

--- AbandonAllQuests.lua
+++ AbandonAllQuests.lua
@@ -12,6 +12,12 @@ local function AbandonAllQuestsHandler()
     for i = 0,getMaxQuests(),1 
     do 
         local id = C_QuestLog.GetQuestIDForLogIndex(i)
+        if(id != 0 )then
+            if(C_QuestLog.CanAbandonQuest(id)) then
+                print('yes')
+
+            end
+        end
         print(id)
     end
 
