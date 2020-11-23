37ca213cf7504976f6a23e89411fa5e3ee97024f
ashler2 <ashley.rt@live.co.uk>
2020-11-23 09:28:26 +0000

    build progress

--- AbandonAllQuests.lua
+++ AbandonAllQuests.lua
@@ -1,1 +1,22 @@
-message("Hello World.")
+SLASH_ABANDON = "/abandonallquests"
+
+
+local function getMaxQuests()
+    return select(1, C_QuestLog.GetNumQuestLogEntries())
+end
+
+local function AbandonAllQuestsHandler()
+
+
+
+    for i = 0,getMaxQuests(),1 
+    do 
+        print(i) 
+    end
+
+end
+
+
+
+
+SlashCmdList['ABANDON'] =  AbandonAllQuestsHandler
